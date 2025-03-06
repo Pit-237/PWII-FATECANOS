3bcaf5107e4184e95cd6830153bf8e79e57fe0e5
Pit-237 <parkergamer237@gmail.com>
2025-03-06 18:26:00 -0300

    Pão

---
+++ 4_include_require/index.php
@@ -0,0 +1,13 @@
+<?php include './header.php';?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center flex-column" style='min-height: 80vh;'>
+                <h1>As Sete Maravilhas do Mundo Moderno</h1>
+                <h3>Conheca as maravilhas do mundo e seus respectivos países</h3>
+                <button class="btn btn-primary">Saiba mais</button>
+            </div>
+        </div>
+    </div>
+
+<?php include './footer.php'; ?>
