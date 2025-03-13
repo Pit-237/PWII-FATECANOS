b1936ea228044e21d203f44f7850941c1bd1f972
parkergamer237@gmail.com <parkergamer237@gmail.com>
2025-03-13 18:25:07 -0300

    Pão

--- 4_include_require/china.php
+++ 4_include_require/china.php
@@ -2,4 +2,35 @@
 
 <h1>Hello Ching-ling</h1>
 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+            <div class="card">
+                <img src="https://aventurasnahistoria.com.br/media/uploads/800px-greatwall_large.jpg" style="width 100%" alt=". . .">
+            </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+                <img src="https://aventurasnahistoria.com.br/media/uploads/800px-greatwall_large.jpg" alt=". . .">
+            </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+                <img src="https://aventurasnahistoria.com.br/media/uploads/800px-greatwall_large.jpg" alt=". . .">
+            </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+                <img src="https://aventurasnahistoria.com.br/media/uploads/800px-greatwall_large.jpg" alt=". . .">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12; style: d-flex; justify-content-center; align-items-center">
+        <h1>Muralhas da china</h1>
+        </div>
+    </div>
+</div>
+
 <?php include './footer.php';?>
--- 4_include_require/header.php
+++ 4_include_require/header.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina Inicial</title>
-    <link href = "./assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" >
-    <link rel ="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
+   
     
 </head>
 <body>
